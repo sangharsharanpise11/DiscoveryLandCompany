cd9fd249ac67edffc6a5a8b09e5a6ff3dc69ea3c
adarsh kiratkudave <sangharsharanpise408@gmail.com>
2020-05-29 04:48:45 +0530

    sliding js working

--- js/Discovery.js
+++ js/Discovery.js
@@ -5,16 +5,17 @@
     });
   });
   
-  $(document).ready(function(){
-    $("#crossBtn").click(function(){
-      console.log("in close btn");
-      $("#panel").slideUp("slow");
-    });
-  });
+  // $(document).ready(function(){
+  //   $("#crossBtn").click(function(){
+  //     console.log("in close btn 1");
+  //     $("#panel").slideUp("slow");
+  //   });
+  // });
 
   $("#crossBtn").on('click', function () {
-    console.log("in close btn");
-    //$("#panel").slideUp("slow");
+    console.log("in close btn 2");
+    $("#panel").slideUp("slow");
+    $(".outerBody").show();
   });
 
   console.log("current window width ",$(window).width());
